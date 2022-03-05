16a5c5b3c4ee3159c1ca1b6c65c6d9b40d566e90
Theo Hussey <husseytg@gmail.com>
2022-03-05 21:24:23 +0000

    added gencad export

---
+++ tests/pcbnew/test_export_gencad.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Theo Hussey
+# License: Apache 2.0
+# Project: KiAuto (formerly kicad-automation-scripts)
+"""
+Tests for 'pcbnew_do export_gencad'
+
+For debug information use:
+pytest-3 --log-cli-level debug
+
+"""
+
+import os
+import sys
+import logging
+# Look for the 'utils' module from where the script is running
+script_dir = os.path.dirname(os.path.abspath(__file__))
+prev_dir = os.path.dirname(script_dir)
+sys.path.insert(0, prev_dir)
+# Utils import
+from utils import context
+sys.path.insert(0, os.path.dirname(prev_dir))
+
+PROG = 'pcbnew_do'
+
+def test_export_gencad(test_dir):
+    """ Simple 3D Viewer test """
+    ctx = context.TestContext(test_dir, 'export_gencad', 'good-project')
+    cmd = [PROG, 'export_gencad', '--output_name', 'good.cad' ]
+    ctx.run(cmd)
+    ctx.expect_out_file('good.cad')
+    ctx.clean_up()
