46bd59db83fb32a9fbff1bdf76cac3f6e4e96be2
Salvador E. Tropea <salvador@inti.gob.ar>
2022-03-08 11:28:35 -0300

    More strict Gencad test

--- tests/pcbnew/test_export_gencad.py
+++ tests/pcbnew/test_export_gencad.py
@@ -9,9 +9,9 @@ For debug information use:
 pytest-3 --log-cli-level debug
 
 """
-
 import os
 import sys
+import re
 # Look for the 'utils' module from where the script is running
 script_dir = os.path.dirname(os.path.abspath(__file__))
 prev_dir = os.path.dirname(script_dir)
@@ -23,10 +23,17 @@ sys.path.insert(0, os.path.dirname(prev_dir))
 PROG = 'pcbnew_do'
 
 
-def test_export_gencad(test_dir):
+def test_export_gencad_1(test_dir):
     """ Generate a GenCAD file with no special options test """
-    ctx = context.TestContext(test_dir, 'export_gencad', 'good-project')
-    cmd = [PROG, 'export_gencad', '--output_name', 'good.cad']
+    ctx = context.TestContext(test_dir, 'export_gencad_1', 'good-project')
+    cmd = [PROG, 'export_gencad', '-f', '-n', '-O', '--output_name', 'good.cad']
     ctx.run(cmd)
     ctx.expect_out_file('good.cad')
+    file = ctx.get_out_path('good.cad')
+    with open(file, 'rt') as f:
+        text = f.read()
+    text = re.sub(r'(USER|DRAWING) "(.*)"', r'\1 ""', text)
+    with open(file, 'wt') as f:
+        f.write(text)
+    ctx.compare_txt('good.cad')
     ctx.clean_up()
